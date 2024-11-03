5c24a678f138f53abf9b07d9239b5753c920e7a2
ElisabettaNale <elisabetta.nale@gmail.com>
2024-11-03 15:09:19 +0100

    bugfix

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { type } = require('os');
 
 module.exports = {
     entry: {
